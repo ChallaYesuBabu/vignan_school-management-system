4754a7a6c3d3ea747b3bbec012e42d8cd7f12d08
ChallaYesuBabu <challayesubabu2@gmail.com>
2026-07-12 19:57:33 +0530

    Added sitemap

--- app.py
+++ app.py
@@ -15,6 +15,33 @@ app.register_blueprint(admin)
 app.register_blueprint(faculty)
 app.register_blueprint(student)
 
+@app.route("/sitemap.xml")
+def sitemap():
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>https://vignan-school-management-system.onrender.com/</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+    <url>
+        <loc>https://vignan-school-management-system.onrender.com/login</loc>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+
+    <url>
+        <loc>https://vignan-school-management-system.onrender.com/gallery</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+
+</urlset>
+"""
+    return Response(xml, mimetype="application/xml")
+
 # =========================
 # HOME ROUTE
 # =========================
